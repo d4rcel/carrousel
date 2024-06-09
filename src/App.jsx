f946cd84e230f60f58fc7a36601289764ba5ab14
d4rcel <daraola05@gmail.com>
2024-06-09 20:54:10 +0100

    Push

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import Carousel from './Carousel';
+import vid1 from './assets/vid1.mp4'
+import vid3 from './assets/vid3.mp4'
+
+function App() {
+  const items = [
+    { type: 'image', src: 'https://via.placeholder.com/800x400?text=Image+1' },
+    { type: 'video', src: vid3 },
+    { type: 'image', src: 'https://via.placeholder.com/800x400?text=Image+2' },
+    { type: 'video', src: vid1 },
+    { type: 'image', src: 'https://via.placeholder.com/800x400?text=Image+3' },
+  ];
+  
+  return (
+    <>
+      <div className=''>
+        <Carousel items={items} />
+      </div>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/Carousel.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './carousel.css'
+
+// Composant pour chaque élément du carrousel
+const CarouselItem = ({ item, isActive, onVideoEnd }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (isActive && item.type === 'video' && videoRef.current) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play().catch(error => {
+        console.error("Error trying to play video automatically:", error);
+      });
+    }
+  }, [isActive, item.type]);
+
+  return (
+    <div className={`carousel-item ${isActive ? 'active' : ''}`}>
+      {item.type === 'image' ? (
+        <img src={item.src} alt="" />
+      ) : (
+        <video 
+          ref={videoRef} 
+          onEnded={onVideoEnd} 
+          src={item.src} 
+          muted 
+          autoPlay
+          controls
+          onError={(e) => console.error('Video eror:', e)}
+          type="video/mp4"
+        />
+      )}
+    </div>
+  );
+};
+
+// Composant principal du carrousel
+const Carousel = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    if (!isPaused) {
+      timeoutRef.current = setTimeout(() => {
+        setActiveIndex((prevIndex) => (prevIndex + 1) % items.length);
+      }, items[activeIndex].type === 'video' ? 0 : 3000);
+    }
+
+    return () => clearTimeout(timeoutRef.current);
+  }, [activeIndex, isPaused, items]);
+
+  const handleVideoEnd = () => {
+    setIsPaused(false);
+    setActiveIndex((prevIndex) => (prevIndex + 1) % items.length);
+  };
+
+  useEffect(() => {
+    if (items[activeIndex].type === 'video') {
+      setIsPaused(true);
+    }
+  }, [activeIndex, items]);
+
+  return (
+    <div className="carousel">
+      {items.map((item, index) => (
+        <CarouselItem
+          key={index}
+          item={item}
+          isActive={index === activeIndex}
+          onVideoEnd={handleVideoEnd}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Carousel
